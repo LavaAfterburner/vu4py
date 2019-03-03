e7baa1b53833bd2781a96a7744fc254325747234
LavaAfterburner <lavaafterburner@gmail.com>
2019-03-03 14:50:10 +0100

    Initial Commit

---
+++ __init__.py
@@ -0,0 +1,17 @@
+"""
+Versatile Utilities Pack for Python
+by LavaAfterburner
+===================================
+V. 2.1
+"""
+
+from vu4py.ULogger import ULogger
+from vu4py.UTkStyle import UTkStyle
+from vu4py.URecursive import URecursive
+from vu4py.UTime import UTime
+from vu4py.UFileSystem import UFileSystem
+
+
+__version__ = "2.1"
+__package__ = "vu4py"
+__all__ = ["ULogger", "UTime", "UFileSystem", "URecursive", "UTkStyle"]
